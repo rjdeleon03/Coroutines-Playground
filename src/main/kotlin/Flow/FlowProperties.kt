760053125d7dbaf0b7097f585dcb0e04a3727846
rjdeleon03 <rjdeleon@up.edu.ph>
2020-02-08 10:23:52 +0800

    Explore flow properties

---
+++ src/main/kotlin/Flow/FlowProperties.kt
@@ -0,0 +1,32 @@
+package Flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
+fun main() {
+    runBlocking {
+        val numbersFlow = sendNewNumbers()
+        println("Flow hasn't started yet...")
+//        numbersFlow.collect {
+//            println("Received $it")
+//        }
+
+        // Cancel flows via coroutine cancellation
+        withTimeoutOrNull(1000L) {
+            numbersFlow.collect { println(it) }
+        }
+    }
+}
+
+//fun sendNewNumbers() = listOf(1, 2, 3).asFlow()
+
+fun sendNewNumbers() = flow {
+    listOf(1, 2, 3).forEach {
+        delay(400L)
+        emit(it)
+    }
+}
