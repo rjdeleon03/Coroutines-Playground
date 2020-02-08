3c44fff454c04550fe4e3f21dc5d01b3fd861888
rjdeleon03 <rjdeleon@up.edu.ph>
2020-02-08 10:16:03 +0800

    Explore ways of creating flows

---
+++ src/main/kotlin/Flow/CreatingFlows.kt
@@ -0,0 +1,25 @@
+package Flow
+
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        sendNumbersFromFlow().collect {
+            println("Received $it")
+        }
+    }
+}
+
+fun sendNumbers() = flow {
+    for (i in 1 .. 10) {
+        emit(i)
+    }
+}
+
+fun sendNumbersFromList() = listOf(1, 2, 3, 4, 5).asFlow()
+
+fun sendNumbersFromFlow() = flowOf(1, 2, 3, 4, 5)
